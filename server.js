382b08655da59ad015bce7d75aa8efe249eb3820
Karim Ayman <86918608+Sayton7@users.noreply.github.com>
2021-07-26 00:50:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const app = express();
 
 //setting up body-parser
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended : false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 //setting up cors
 const cors = require('cors');
+const { networkInterfaces } = require('os');
 app.use(cors());
 
 //directing to the main project folder
@@ -22,3 +23,20 @@ function listening() {
     console.log("server running");
     console.log(`running on port: ${port}`);
 }
+
+//setting the POST route
+const weatherData = [];
+
+app.post('/info', info);
+
+function info(req, res) {
+
+    newEntry = {
+        temp: req.body.temp,
+        feel: req.body.feel
+    }
+
+    weatherData.push(newEntry);
+    res.send(weatherData);
+    console.log(weatherData);
+};
